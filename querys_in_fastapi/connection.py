aff14033982167247338fbd4b6ddb899059d0db0
Daniel Zemtsovski <d0556789739@gmail.com>
2026-03-17 15:34:14 +0200

    add connection

--- querys_in_fastapi/connection.py
+++ querys_in_fastapi/connection.py
@@ -1,25 +1,32 @@
 import pymysql
+import os
+from dotenv import load_dotenv
+from logger import log_event
 
-config = {
-    "host": "localhost",
-    "port": 3306,
-    "user": "root",
-    "password": "root",
-    "database": "digital_hunter"
-    }
 
-try:
-    conn = pymysql.connect(**config)
-    cur = conn.cursor()
-    query = "SHOW TABLES"     #"select * from attacks"
-    cur.execute(query)
-    s = cur.fetchall()
-    for r in s:
-        print(r)
+load_dotenv()
 
-except Exception as a:
-    print(f"{a}")
+class Connection:
+    def __init__(self):
+        log_event("INFO", "Initializing Database configuration")
+        self.config = {
+            "host": os.getenv("DB_HOST", "localhost"),
+            "port": int(os.getenv("DB_PORT", 3306)),
+            "user": os.getenv("DB_USER", "root"),
+            "password": os.getenv("DB_PASSWORD"),
+            "database": os.getenv("DB_NAME", "digital_hunter"),
+            "cursorclass": pymysql.cursors.DictCursor 
+        }
+        log_event("INFO", f"Database config loaded for host: {self.config['host']}, user: {self.config['user']}")
 
-finally:
-    conn.close()
+    def get_connection(self):
+        try:
+            connection = pymysql.connect(**self.config)
+            log_event("INFO", "Successfully established connection to MySQL")
+            return connection
+        except Exception as e:
+            log_event("ERROR", f"Failed to connect to MySQL: {str(e)}")
+            raise e
 
+
+connection = Connection()
